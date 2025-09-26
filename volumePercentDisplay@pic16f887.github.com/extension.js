5969d8e096a4612b0d91169c3b2349a96445a834
Karl Wulfert <pic16f877.ccs@emailn.de>
2025-09-26 21:32:55 +0200

    Add horizontal volume

--- volumePercentDisplay@pic16f887.github.com/extension.js
+++ volumePercentDisplay@pic16f887.github.com/extension.js
@@ -1,3 +1,4 @@
+import Clutter from 'gi://Clutter';
 import GObject from 'gi://GObject';
 import St from 'gi://St';
 
@@ -5,12 +6,15 @@ import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import { Extension, InjectionManager } from 'resource:///org/gnome/shell/extensions/extension.js';
 import { OsdWindowManager as OsdWindowManagerOrig, OsdWindow } from "resource:///org/gnome/shell/ui/osdWindow.js";
 
-export default class ExampleExtension extends Extension {
+export default class VolumePercentExtension extends Extension {
     enable() {
+        this._settings = this.getSettings();
         this._injectionManager = new InjectionManager();
         this._osdWindows = [];
         Main.layoutManager.connect('monitors-changed',
         this._monitorsChanged.bind(this));
+
+        this._percent_label_position = this._settings.get_boolean('position-right-bottom');
         this._monitorsChanged();
 
         this._injectionManager.overrideMethod(OsdWindowManagerOrig.prototype, '_showOsdWindow',
@@ -30,7 +34,7 @@ export default class ExampleExtension extends Extension {
     _monitorsChanged() {
         for (let i = 0; i < Main.layoutManager.monitors.length; i++) {
             if (this._osdWindows[i] === undefined)
-                this._osdWindows[i] = new VolumePercentOsdWindow(i);
+                this._osdWindows[i] = new VolumePercentOsdWindow(i, this._settings);
         }
 
         for (let i = Main.layoutManager.monitors.length; i < this._osdWindows.length; i++) {
@@ -54,12 +58,76 @@ export default class ExampleExtension extends Extension {
 
 export const VolumePercentOsdWindow = GObject.registerClass(
     class VolumePercentOsdWindow extends OsdWindow {
-    _init(monitorIndex) {
+    _init(monitorIndex, settings) {
         super._init(monitorIndex)
+        this._settings = settings;
+        this._percent_label_position = this._settings.get_boolean('position-right-bottom');
+
+        this._settings.connect('changed::position-right-bottom', this._positionMoved.bind(this));
+
         this._label_percent = new St.Label();
         this._vbox.add_style_class_name('osd-window-box');
         this._label.add_style_class_name('osd-window-label');
-        this._vbox.add_child(this._label_percent);
+
+        if (this._percent_label_position) {
+            this._vbox.add_child(this._label_percent);
+        } else {
+            this._empty_box = new St.BoxLayout({
+                orientation: Clutter.Orientation.VERTICAL,
+                y_align: Clutter.ActorAlign.CENTER,
+                height: this._label.height,
+            });
+            this._vbox.add_child(this._empty_box);
+
+            this._right_box = new St.BoxLayout({
+                orientation: Clutter.Orientation.VERTICAL,
+                y_align: Clutter.ActorAlign.CENTER,
+            });
+            this._right_box.add_child(this._label_percent);
+
+            this._hbox.add_child(this._right_box);
+        }
+    }
+
+    _positionMoved() {
+        this._percent_label_position = this._settings.get_boolean('position-right-bottom');
+
+        if (this._percent_label_position) {
+            if (this._vbox.get_last_child() === this._empty_box) {
+                this._vbox.remove_child(this._empty_box);
+                this._empty_box.destroy();
+            }
+
+            if (this._right_box.get_last_child() === this._label_percent) {
+                this._right_box.remove_child(this._label_percent);
+            }
+
+            if (this._hbox.get_last_child() === this._right_box) {
+                this._hbox.remove_child(this._right_box);
+                this._right_box.destroy();
+            }
+
+            this._vbox.add_child(this._label_percent);
+        } else {
+            if (this._vbox.get_last_child() === this._label_percent) {
+                this._vbox.remove_child(this._label_percent);
+            }
+
+            this._empty_box = new St.BoxLayout({
+                orientation: Clutter.Orientation.VERTICAL,
+                y_align: Clutter.ActorAlign.CENTER,
+                height: this._label.height,
+            });
+            this._vbox.add_child(this._empty_box);
+
+            this._right_box = new St.BoxLayout({
+                orientation: Clutter.Orientation.VERTICAL,
+                y_align: Clutter.ActorAlign.CENTER,
+            });
+            this._right_box.add_child(this._label_percent);
+
+            this._hbox.add_child(this._right_box);
+        }
     }
 
     _setLabelPercent(label) {
