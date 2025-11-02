1e13579234c7b6a9614fb4cb65143a7fe44ab250
Karl Wulfert <pic16f877.ccs@emailn.de>
2025-11-02 21:51:08 +0100

    Add src directory

---
+++ src/prefs.js
@@ -0,0 +1,32 @@
+import Adw from 'gi://Adw';
+import Gio from 'gi://Gio';
+import Gtk from 'gi://Gtk';
+
+import {ExtensionPreferences,
+    gettext as _ } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+
+export default class MonitorSmartSaverPreferences extends ExtensionPreferences {
+    fillPreferencesWindow(window) {
+        window._settings = this.getSettings();
+
+        const page = new Adw.PreferencesPage({
+            title: null,
+            icon_name: null,
+        });
+        window.add(page);
+
+        const volumePercentPosition = new Adw.PreferencesGroup({ title: _('Position')});
+        volumePercentPosition.set_separate_rows?.(true);
+        page.add(volumePercentPosition);
+
+        const volumePositionSwitchRow = new Adw.SwitchRow({
+            title: _('Volume percent position'),
+            subtitle: _('Volume percent position right or bottom'),
+        });
+
+        volumePercentPosition.add(volumePositionSwitchRow);
+
+        window._settings.bind('position-right-bottom', volumePositionSwitchRow, 'active',
+        Gio.SettingsBindFlags.DEFAULT);
+    }
+}
